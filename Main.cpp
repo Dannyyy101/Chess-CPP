0d729124aa9d3644a5b61b443e5e6a69642bc8c7
Daniel Stöcklein <3005650@stud.hs-mannheim.de>
2024-02-26 12:54:06 +0100

    add main

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Daniel Stöcklein on 21.02.24.
+//
+
+#include <iostream>
+
+#include "./presenter/Session.h"
+
+using namespace std;
+
+int main(){
+    Session session;
+    session.run();
+
+    return 0;
+}
